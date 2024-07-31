8559cb90a6810f1d54f0870d5654bda865e51bd6
kimsiyeon0223 <ksyeon0223@gmail.com>
2024-07-31 22:14:29 +0900

    server 주석작성

--- html5-websocket/server/server.js
+++ html5-websocket/server/server.js
@@ -1,7 +1,10 @@
-//1
+//1 : socket.io를 프로젝트에 추가합니다. Server라는 생성자를 이용해 소켓 서버를 생성합니다.
 const { Server } = require("socket.io");
 
 //2
+//new Server를 이용해 5000번 포르를 가진 소켓 서버를 만듭니다. 
+//5000번 포트 자리에 http 서버 객체를 만들 수도 있습니다.
+//http 서버를 이용하지 않기 때문에 임의 포트 번호로 대체했습니다.
 const io = new Server("5000", {
   cors: {
     origin: "http://localhost:3000",
